41987dff6a3dbcc95decbbceb26b4f2bd3e6a748
Shawn T O'Neil <oneilsh@gmail.com>
2026-05-11 13:57:35 -0400

    cloud lda eval tests

--- analysis/cloud/run_lda_w_eval.sh
+++ analysis/cloud/run_lda_w_eval.sh
@@ -7,7 +7,7 @@ SAVE_DIR=/mnt/gcs/$BUCKET/runs/$RUN_ID
 make lda-bq-smoke \
     SAVE_DIR=$SAVE_DIR \
     SAVE_INTERVAL=10 \
-    LDA_BQ_ARGS='--K 50 --max-iter 150 --person-mod 50 --vocab-size 5000 --min-df 10 --holdout-fraction 0.1 --holdout-seed 42 --tau0 64 --kappa 0.7 --print-topics-every 1'
+    LDA_BQ_ARGS='--K 50 --max-iter 10 --person-mod 50 --vocab-size 5000 --min-df 10 --holdout-fraction 0.1 --holdout-seed 42 --tau0 64 --kappa 0.7 --print-topics-every 1'
 
 
 make eval-bq-coherence CHECKPOINT=$SAVE_DIR
