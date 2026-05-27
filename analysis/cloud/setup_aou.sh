18bbb30e5939d952c87b8ce0eb6130d3ed17a2c6
Shawn T O'Neil <oneilsh@gmail.com>
2026-05-27 15:04:09 -0400

    fix aou setup script

--- analysis/cloud/setup_aou.sh
+++ analysis/cloud/setup_aou.sh
@@ -5,6 +5,7 @@ sudo curl -fsSL https://pyenv.run | bash
 pip3 install --user poetry
 curl https://pyenv.run | bash
 
+cd ~/
 git clone https://github.com/oneilsh/.dotfiles
 cd ~/.dotfiles
 ./install
